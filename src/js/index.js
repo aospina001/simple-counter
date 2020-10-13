d474c6fe79ab56f2f85e8aee485892a0fb010729
aospina001 <aospina001@gmail.com>
2020-10-13 20:01:53 +0000

    Counter finalized

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,65 @@
-//import react into the bundle
+// //import react into the bundle
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// //include bootstrap npm library into the bundle
+// import "bootstrap";
+
+// //include your index.scss file into the bundle
+// import "../styles/index.scss";
+
+// //import your own components
+// import { Home } from "./component/home.js";
+
+// //render your react application
+// ReactDOM.render(<Home />, document.querySelector("#app"));
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
-import "bootstrap";
+class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleOne = this.handleOne.bind(this);
+		this.handleMinusOne = this.handleMinusOne.bind(this);
+		this.handleReset = this.handleReset.bind(this);
+		this.state = {
+			count: 0
+		};
+	}
+	handleOne() {
+		this.setState(prevState => {
+			return {
+				count: prevState.count + 1
+			};
+		});
+	}
+	handleMinusOne() {
+		this.setState(prevState => {
+			return {
+				count: prevState.count - 1
+			};
+		});
+	}
 
-//include your index.scss file into the bundle
-import "../styles/index.scss";
+	handleReset() {
+		this.setState(() => {
+			return {
+				count: 0
+			};
+		});
+	}
 
-//import your own components
-import { Home } from "./component/home.js";
+	render() {
+		return (
+			<div>
+				<h1>Count: {this.state.count}</h1>
+				<button onClick={this.handleOne}>+1</button>
+				<button onClick={this.handleMinusOne}>-1</button>
+				<button onClick={this.handleReset}>reset</button>
+			</div>
+		);
+	}
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Counter />, document.getElementById("app"));
